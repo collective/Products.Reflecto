cde015f99644a2069ae694db304ff60c4913c937
wichert <wichert@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2008-03-07 09:48:03 +0000

    Setup basic structure for the Reflecto egg

---
+++ setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+import os
+
+version = '2.0'
+
+setup(name='Products.Reflecto',
+      version=version,
+      description="Access the filesystem from Plone",
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Framework :: Plone",
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+      keywords='plone filesystem',
+      author='Jarn AS',
+      author_email='support@jarn.com',
+      url='http://svn.plone.org/svn/plone/plone.example',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['Products'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
